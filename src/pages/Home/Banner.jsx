75139b85a74fc2881a19d0a5ca96c8ce0ba8d8c1
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-09-23 00:37:43 +0600

    deploy: production test

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
   <div className="hero-overlay"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <h1 className="mb-5 text-5xl font-bold">Hello World</h1>
       <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.

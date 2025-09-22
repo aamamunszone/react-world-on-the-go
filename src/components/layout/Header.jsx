1e497d458e7dbcb0e63e55c2fa53e77e7581cd57
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-09-23 01:11:08 +0600

    deploy: production test again

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-2xl text-[orangered]">Countries</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
